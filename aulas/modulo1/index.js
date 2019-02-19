3de0d8a7f8cfdb341e8a9ecf72c5f11df4e0cfd1
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-02-18 21:46:20 -0300

    Aqui adicionamos um Middleware que intercepta a requisição, cria uma nova variável (appName) e repassa a outros middlewares que são executados após ele

--- aulas/modulo1/index.js
+++ aulas/modulo1/index.js
@@ -1,7 +1,20 @@
 const express = require("express"); //Importamos o express para criar nosso server
 const app = express();
+
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  );
+
+  req.appName = "GoNode";
+
+  return next();
+};
+
+app.use(logMiddleware);
+
 app.get("/", (req, res) => {
-  return res.send(`Bem vindo, ${req.query.name}`);
+  return res.send(`Bem vindo ${req.appName}, ${req.query.name}`);
 });
 
 app.get("/nome/:name", (req, res) => {
