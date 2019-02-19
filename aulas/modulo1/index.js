028cc571f60abb2858c6d9c2dd756510d9333148
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-02-18 21:32:02 -0300

    Nesse commit aprendemos como passar parametros para o Express e também instalamos o nodemon no projeto

--- aulas/modulo1/index.js
+++ aulas/modulo1/index.js
@@ -1,10 +1,10 @@
 const express = require("express"); //Importamos o express para criar nosso server
 const app = express();
 app.get("/", (req, res) => {
-  return res.send("Hello World");
+  return res.send(`Bem vindo, ${req.query.name}`);
 });
 
-app.get("/login", (req, res) => {
-  return res.send("Login");
+app.get("/nome/:name", (req, res) => {
+  return res.send(`Welcome, ${req.params.name}`);
 });
 app.listen(3000);
