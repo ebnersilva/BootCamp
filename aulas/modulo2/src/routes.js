1d0cbf59d144b24bdc465c974504587aa538442d
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-02-21 13:25:36 -0300

    Nesse commit criamos a lógica para horários disponíveis

--- aulas/modulo2/src/routes.js
+++ aulas/modulo2/src/routes.js
@@ -12,6 +12,7 @@ const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
 const AppointmentController = require('./app/controllers/AppointmentController')
+const AvailableController = require('./app/controllers/AvailableController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -28,12 +29,14 @@ routes.post('/signin', SessionController.store)
 routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
-routes.get('/app/appointments/new/:provider', AppointmentController.create)
-
 routes.use('/app', authMiddleware)
 
 routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
+
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
