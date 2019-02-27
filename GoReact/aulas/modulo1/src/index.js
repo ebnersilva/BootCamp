a2cfea22da5f26e6248028e271b9da027b84f421
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-02-27 14:45:34 -0300

    GoReact - Aprendemos a mexer com estilos css Sass e importar diretamente no javascript

--- GoReact/aulas/modulo1/src/index.js
+++ GoReact/aulas/modulo1/src/index.js
@@ -3,23 +3,12 @@ import { render } from "react-dom";
 
 import Button from "./Button";
 
+import "./style.scss";
+
 class App extends Component {
   state = {
     counter: 0
   };
-
-  //Chamadas a Api, listagem etc, Iniciar EventListeners
-  componentDidMount() {}
-
-  //Intermediador da modificação do state e renderização na tela
-  //Troca o estado mas podemos bloquear a renderização
-  shouldComponentUpdate(nextProps, nextState) {}
-
-  //Executa depois de sofrer atualização do state e da renderização
-  componentDidUpdate(prevProps, prevState) {}
-
-  //Ouvindo uma digitação ou resize de janela
-  componentWillUnmount() {}
 
   handleClick = () => {
     this.setState({ counter: this.state.counter + 1 });
