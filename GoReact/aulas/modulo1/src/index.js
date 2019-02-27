47a662e8ed0abc4fd4c3cc71e395bdd397415909
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-02-27 14:31:49 -0300

    GoReact - Aprendemos os ciclos de vida do React

--- GoReact/aulas/modulo1/src/index.js
+++ GoReact/aulas/modulo1/src/index.js
@@ -8,6 +8,19 @@ class App extends Component {
     counter: 0
   };
 
+  //Chamadas a Api, listagem etc, Iniciar EventListeners
+  componentDidMount() {}
+
+  //Intermediador da modificação do state e renderização na tela
+  //Troca o estado mas podemos bloquear a renderização
+  shouldComponentUpdate(nextProps, nextState) {}
+
+  //Executa depois de sofrer atualização do state e da renderização
+  componentDidUpdate(prevProps, prevState) {}
+
+  //Ouvindo uma digitação ou resize de janela
+  componentWillUnmount() {}
+
   handleClick = () => {
     this.setState({ counter: this.state.counter + 1 });
   };
