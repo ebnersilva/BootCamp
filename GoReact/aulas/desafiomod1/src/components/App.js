d4aea41345b99032a4293325d8e351345c3f2e23
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-02-27 16:47:36 -0300

    GoReact - DesafioMod01 - Desafio finalizado de acordo com as classes da RocketSeat

--- GoReact/aulas/desafiomod1/src/components/App.js
+++ GoReact/aulas/desafiomod1/src/components/App.js
@@ -4,8 +4,44 @@ import Header from './Header';
 import Post from './Post';
 
 export default class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        name: 'Diego Fernandes',
+        avatar: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4',
+        time: 'há 3 min',
+        body:
+          'Mauris non Stempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis',
+      },
+      {
+        id: 2,
+        name: 'Rocketseat',
+        avatar: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4',
+        time: 'há 30 min',
+        body:
+          'Pellentesque, lacinia dui sed, ultricies sapien. Pellentesque orci lectus, consectetur vel posuere posuere, rutrum eu ipsum.',
+      },
+      {
+        id: 3,
+        name: 'Rocketseat',
+        avatar: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4',
+        time: 'há 38 min',
+        body:
+          'Pellentesque, lacinia dui sed, ultricies sapien. Pellentesque orci lectus, consectetur vel posuere posuere, rutrum eu ipsum.',
+      },
+    ],
+  };
+
   render() {
     const { posts } = this.state;
-    return <h1>Olá Mundo</h1>;
+    return (
+      <div>
+        <Header />
+        <div className="post-container">
+          {posts && posts.map(post => <Post key={post.id} data={post} />)}
+        </div>
+      </div>
+    );
   }
 }
