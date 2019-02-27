3f22e5d1a9898d0d8fa714922d23dabf51504af1
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-02-27 15:09:11 -0300

    GoReact - Configuramos o Eslint e o Prettier

--- GoReact/aulas/modulo1/src/index.js
+++ GoReact/aulas/modulo1/src/index.js
@@ -1,28 +1,30 @@
-import React, { Component, Fragment } from "react";
-import { render } from "react-dom";
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
 
-import Button from "./Button";
+import Button from './Button';
 
-import "./style.scss";
+import './style.scss';
 
 class App extends Component {
   state = {
-    counter: 0
+    counter: 0,
   };
 
   handleClick = () => {
-    this.setState({ counter: this.state.counter + 1 });
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
   };
 
   render() {
+    const { counter } = this.state;
     return (
       <Fragment>
         <h1>Hello RocketSeat</h1>
-        <h2>{this.state.counter}</h2>
+        <h2>{counter}</h2>
         <Button onClick={this.handleClick}>Enviar</Button>
       </Fragment>
     );
   }
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById('app'));
