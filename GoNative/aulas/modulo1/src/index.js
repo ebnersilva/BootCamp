3daf728579614ca9c3434535f06f61a1907c8638
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-05 18:48:37 -0300

    GoNative - modulo1 - Aprendemos sobre o ciclo de vida ro React

--- GoNative/aulas/modulo1/src/index.js
+++ GoNative/aulas/modulo1/src/index.js
@@ -1,36 +1,49 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
-import Todo from "./components/Todo";
-
 export default class App extends Component {
   state = {
-    todos: [
-      {
-        id: 1,
-        text: "Fazer Café"
-      },
-      {
-        id: 2,
-        text: "Estudar o Gonative"
-      }
-    ]
+    counter: 0
   };
 
-  addTodo = () => {
-    this.setState({
-      todos: [...this.state.todos, { id: Math.random(), text: "Novo Todo" }]
-    });
+  // Quando o componente é renderizado
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ text: "Hello World" });
+    }, 3000);
+  }
+
+  // Executado antes do componentDidMount quanto antes de cada atualização
+  // nextProps: proximo valor atualizado, prevState: state atual antes de atualizar
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return { text: nextProps.text };
+  }
+
+  // Executado a cada atualização antes mesmo do componente atualizar
+  // nextProps: , nextState:
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.counter < 5; //Aqui só renderizará no valor menor que 5 mas no state será atualizado
+  }
+  handleAddCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
   };
+
+  // Executado após que o state e o render são efetuados
+  // Conseguimos pegar as props e os estados antes de ser atualizado
+  componentDidUpdate(prevProps, prevState) {
+    // Código
+  }
+
+  // Executado quando o componente será desmontado
+  componentWillUnmount() {
+    //Código
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        {this.state.todos.map(todo => (
-          <Todo key={todo.id} title={todo.text} />
-        ))}
-
-        <Button title="Adicionar Todo" onPress={this.addTodo} />
+        <Button onPress={this.handleAddCounter} title="Add" />
+        <Text>{this.state.counter}</Text>
       </View>
     );
   }
