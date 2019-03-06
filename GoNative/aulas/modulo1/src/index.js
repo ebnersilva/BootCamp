4d74d478c7abfb8ca5fcab57d608ab07a08c1fb3
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-06 14:16:05 -0300

    GoNative - modulo1 - Configuramos o EsLint com o Prettier para melhorar o refatoramento de código no projeto

--- GoNative/aulas/modulo1/src/index.js
+++ GoNative/aulas/modulo1/src/index.js
@@ -1,33 +1,54 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import './config/ReactotronConfig';
 
-import Todo from "./components/Todo";
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Todo from './components/Todo';
+
+const bgColor = '#f00';
+const bgColor1 = '#333';
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: bgColor,
+    height: 80,
+    margin: 10,
+    marginHorizontal: 20,
+    marginVertical: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    transform: [{ rotateZ: '20deg' }],
+    width: 80,
+  },
+  container: {
+    alignContent: 'center',
+    backgroundColor: bgColor1,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+});
 
 export default class App extends Component {
   state = {
     todos: [
       {
         id: 1,
-        text: "Fazer café"
+        text: 'Fazer café',
       },
       {
         id: 2,
-        text: "Estudar GoNative"
-      }
+        text: 'Estudar GoNative',
+      },
     ],
-    counter: 0
-  };
-
-  handleAddTodo = () => {
-    this.setState({
-      todos: [...this.state.todos, { id: Math.random(), text: "Novo Todo" }]
-    });
+    counter: 0,
   };
 
   // Quando o componente é renderizado
   componentDidMount() {
     setTimeout(() => {
-      this.setState({ text: "Hello World" });
+      this.setState({ text: 'Hello World' });
     }, 3000);
   }
 
@@ -40,11 +61,9 @@ export default class App extends Component {
   // Executado a cada atualização antes mesmo do componente atualizar
   // nextProps: , nextState:
   shouldComponentUpdate(nextProps, nextState) {
-    return nextState.counter < 5; //Aqui só renderizará no valor menor que 5 mas no state será atualizado
+    return nextState.counter < 5;
+    // Aqui só renderizará no valor menor que 5 mas no state será atualizado
   }
-  handleAddCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
 
   // Executado após que o state e o render são efetuados
   // Conseguimos pegar as props e os estados antes de ser atualizado
@@ -54,8 +73,20 @@ export default class App extends Component {
 
   // Executado quando o componente será desmontado
   componentWillUnmount() {
-    //Código
+    // Código
   }
+
+  handleAddTodo = () => {
+    const { todos } = this.state;
+    this.setState({
+      todos: [...todos, { id: Math.random(), text: 'Novo Todo' }],
+    });
+  };
+
+  handleAddCounter = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
 
   render() {
     return (
@@ -77,25 +108,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#333",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignContent: "center"
-  },
-  box: {
-    width: 80,
-    height: 80,
-    backgroundColor: "#f00",
-    margin: 10,
-    marginHorizontal: 20,
-    marginVertical: 20,
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-    transform: [{ rotateZ: "20deg" }]
-  }
-});
