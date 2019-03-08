6e86d1e6ad87c404975490b65988e8f807f70f5c
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-08 14:48:24 -0300

    GoNative - modulo2 - Definimos a rota inicial da aplicação ou seja, se o usuário estiver logado redireciona direto para os repositórios ... se não estiver logado redireciona para a tela de Welcome

--- GoNative/aulas/modulo2/src/pages/Repositories/index.js
+++ GoNative/aulas/modulo2/src/pages/Repositories/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 
 // import styles from './styles';
 
-const Repositories = () => <View />;
+const Repositories = () => (
+  <View style={{ flex: 1, backgroundColor: '#f34' }}>
+    <Text style={{ flex: 1, color: '#fff5' }}>Repositórios</Text>
+  </View>
+);
 
 export default Repositories;
