8b538e6ec3f84bc189a888ee88080b5e71443a39
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-08 20:48:29 -0300

    GoNative - modulo2 - Criamos o menu de baixo da aplicação com com 2 icones para seleção das páginas

--- GoNative/aulas/modulo2/src/pages/Welcome/index.js
+++ GoNative/aulas/modulo2/src/pages/Welcome/index.js
@@ -45,7 +45,7 @@ export default class Welcome extends Component {
       await this.checkUserExists(username);
       await this.saveUser(username);
 
-      navigation.navigate('Repositories');
+      navigation.navigate('User');
     } catch (error) {
       this.setState({ loading: false, error: true });
     }
