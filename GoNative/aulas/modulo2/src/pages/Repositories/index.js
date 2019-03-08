18c7c0770e4b54a6644367bacdfcfd689d1bcf80
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-08 15:18:59 -0300

    GoNative - modulo2 - Criamos o componente Header e ja vinculamos a página de repositórios... esse componente será utilizado em outras páginas... também estilizamos o mesmo... mudamos também a cor root padrão do IOS que antes era preto e agora mudamos para branco (whiteColor) ... utilizamos também a lib: react-native-status-bar-height que com ela conseguimos pegar a altura da status bar do dispositivo android ou IOS e garantir que utilizaremos sempre a area util do aparelho

--- GoNative/aulas/modulo2/src/pages/Repositories/index.js
+++ GoNative/aulas/modulo2/src/pages/Repositories/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { View, Text, AsyncStorage } from 'react-native';
+import { View } from 'react-native';
 
+import Header from '~/components/Header';
 // import styles from './styles';
 
 const Repositories = () => (
-  <View style={{ flex: 1, backgroundColor: '#f34' }}>
-    <Text style={{ flex: 1, color: '#fff5' }}>Repositórios</Text>
+  <View>
+    <Header title="Repositórios" />
   </View>
 );
 
