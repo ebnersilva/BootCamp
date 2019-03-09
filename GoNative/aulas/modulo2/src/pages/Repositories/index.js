887c2123c476312e45378a8b9c3a7614475951e4
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-08 21:07:40 -0300

    GoNative - modulo2 - Criamos a consulta da Api do Github a partir de o usuário logado... também criamos um activityIndicator em cima dos dados que vem da API

--- GoNative/aulas/modulo2/src/pages/Repositories/index.js
+++ GoNative/aulas/modulo2/src/pages/Repositories/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { View, AsyncStorage, Text, ActivityIndicator } from 'react-native';
+import api from '~/services/api';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
-// import styles from './styles';
-
-const Repositories = () => (
-  <View>
-    <Header title="Repositórios" />
-  </View>
-);
+import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
 
@@ -19,8 +14,36 @@ TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Repositories.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
+export default class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
 
-export default Repositories;
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    const { data } = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data, loading: false });
+  }
+
+  renderList = () => {
+    const { data } = this.state;
+    data.map(repo => <Text>{repo.name}</Text>);
+  }
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <View>
+        <Header title="Repositórios" />
+        { loading ? <ActivityIndicator style={styles.loading} /> : this.renderList }
+      </View>
+    );
+  }
+}
