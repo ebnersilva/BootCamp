2c793460328e5ac7488b9e55fa063514d1807a61
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-11 12:23:36 -0300

    GoNative - desafiomod2 - Criamos o Input da primeira página

--- GoNative/aulas/desafiomod2/src/pages/Repositories/index.js
+++ GoNative/aulas/desafiomod2/src/pages/Repositories/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View, TextInput, TouchableOpacity, StatusBar,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '../../components/Header';
 
 import styles from './styles';
 
-const Repositories = () => (
-  <View style={styles.container}>
-    <Header title="Repositórios" />
-    <Text>Welcome</Text>
-  </View>
-);
+export default class Repositories extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Repositories;
+  render() {
+    const { repositoryInput } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <Header title="Repositórios" />
+        <View style={styles.form}>
+          <View style={styles.containerInput}>
+            <TextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              placeholder="Adicionar novo repositório"
+              style={styles.formInput}
+              placeholderTextColor={styles.placeholderColor.color}
+              value={repositoryInput}
+              onChangeText={text => this.setState({ repositoryInput: text })}
+            />
+            <TouchableOpacity onPress={() => {}}>
+              <Icon name="plus" size={20} style={styles.formIcon} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
--- GoNative/aulas/desafiomod2/src/pages/Repositories/styles.js
+++ GoNative/aulas/desafiomod2/src/pages/Repositories/styles.js
@@ -1,10 +1,34 @@
 import { StyleSheet } from 'react-native';
-import { colors } from '../../styles';
+import { colors, metrics } from '../../styles';
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.lighter,
     flex: 1,
+  },
+  containerInput: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  form: {
+    borderBottomColor: colors.light,
+    borderBottomWidth: 1,
+    padding: metrics.basePadding,
+  },
+  formIcon: {
+    color: colors.dark,
+  },
+  formInput: {
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    color: colors.darker,
+    flex: 1,
+    fontSize: 14,
+    marginRight: metrics.baseMargin,
+    padding: metrics.basePadding / 2,
+  },
+  placeholderColor: {
+    color: colors.regular,
   },
 });
 
