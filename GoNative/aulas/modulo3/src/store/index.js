80c32cd173cfef605ed313af203677824d6c1409
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-12 16:47:25 -0300

    GoNative - modulo3 - Integramos o Reactotron com o Redux... agora conseguimos debuggar a aplicação e ver todas as actions com payloads da nossa aplicação

--- GoNative/aulas/modulo3/src/store/index.js
+++ GoNative/aulas/modulo3/src/store/index.js
@@ -1,29 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
-// Reducer
-const INITIAL_STATE = [
-  { id: 1, text: 'Fazer café', completed: false },
-  { id: 2, text: 'Estudar React Native', completed: true },
-  { id: 3, text: 'Estudar Redux', completed: false },
-];
-function reducer(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [...state, { id: Math.random(), text: action.payload.text, completed: false }];
-    case 'MARK_AS_COMPLETED':
-      return state.map(todo => (todo.id === action.payload.id ? { ...todo, completed: !todo.completed } : todo));
-    default:
-      return state;
-  }
-}
+import todos from './reducers/todos';
 
-// Actions
-// { type: 'ADD_TODO', text: 'Novo Todo' }
-// { type: 'MARK_AS_COMPLETED', id: 3 }
-
-// Adicionar um Todo
-// Marcar todo como completo
-
-const store = createStore(reducer);
-
-export default store;
+export default (__DEV__
+  ? createStore(todos, compose(console.tron.createEnhancer()))
+  : createStore(todos));
