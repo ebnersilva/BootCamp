e55441b39285b9befc9f9e2992a41873ccafd801
Ebner Silva <ebner.suporte@hotmail.com>
2019-03-14 16:28:04 -0300

    GoNative - desafiomod3 - Nesse commit está tudo funcionando perfeitamente no Android... também foi corrigido o problema no arquivo AbstractEventEmitter.java conforme link: https://github.com/nitaliano/react-native-mapbox-gl/pull/1399/commits/bef7dbcde1a4a0a49c78b06433c914acfe80ef15

--- GoNative/aulas/desafiomod3/App.js
+++ GoNative/aulas/desafiomod3/App.js
@@ -1,30 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, View, PermissionsAndroid } from 'react-native';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+Mapbox.setAccessToken('pk.eyJ1IjoiZWJuZXJkZWF0aCIsImEiOiJjanA4aGJyNTkxNHBuM3JxajB2ajJsdXB5In0.nLW_k2u_mbv2ytZxPbuICA');
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    user: {
+      latitude = 0,
+      longitude = 0,
+    }
+  }
+
+  renderAnnotations () {
+    return (
+      <Mapbox.PointAnnotation
+        key='pointAnnotation'
+        id='pointAnnotation'
+        coordinate={[-49.372980, -25.554312]}>
+
+        <View style={styles.annotationContainer}>
+          <View style={styles.annotationFill} />
+        </View>
+        <Mapbox.Callout title='Ponto fixo no mapa' />
+      </Mapbox.PointAnnotation>
+    )
+  }
+
+  _onLongPress(e) {
+    const [latitudeActual, longitudeActual] = e.geometry.coordinates;
+
+    this.setState(
+      {
+        user: {latitude: latitudeActual, longitude: longitudeActual
+      }
+    });
+
+    const { latitude, longitude } = this.state.user;
+
+    //console.log(latitude, longitude);
+  }
+
+  componentDidMount(){
+    PermissionsAndroid.requestMultiple(
+              [PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+              PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION],
+              {
+                  title: 'Permissão de Localização',
+              message: 'O App precisa de permissão de localização para encontrar sua posição'
+          }
+      ).then(granted => {
+          //console.log(granted);
+          resolve();
+      }).catch(err => {
+          //console.warn(err);
+          //reject(err);
+      });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Mapbox.MapView
+        onLongPress={this._onLongPress}
+        styleURL={Mapbox.StyleURL.Dark}
+        zoomLevel={15}
+        centerCoordinate={[-49.6451598, -27.2177659]}
+        style={styles.container}
+      >
+
+      </Mapbox.MapView>
     );
   }
 }
@@ -32,18 +76,20 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  annotationContainer: {
+    width: 30,
+    height: 30,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 15,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'orange',
+    transform: [{ scale: 0.6 }],
   },
 });
