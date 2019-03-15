22de9c9b4f44aaf4be3a5c85719605133e2eced8
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-15 11:50:32 -0300

    GoNative - desafiomod3 - No Commit anterior deixamos funcionando com o mapbox no android mas isso apresentou muitos problemas... então mudamos de lib para o react-native-maps e agora está funcionando a lib no android e no ios por enquanto

--- GoNative/aulas/desafiomod3/App.js
+++ GoNative/aulas/desafiomod3/App.js
@@ -1,95 +1,62 @@
-import React, {Component} from 'react';
-import { StyleSheet, View, PermissionsAndroid } from 'react-native';
-import Mapbox from '@mapbox/react-native-mapbox-gl';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid
+} from "react-native";
+import MapView from "react-native-maps";
 
-Mapbox.setAccessToken('pk.eyJ1IjoiZWJuZXJkZWF0aCIsImEiOiJjanA4aGJyNTkxNHBuM3JxajB2ajJsdXB5In0.nLW_k2u_mbv2ytZxPbuICA');
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    flex: 1
+  }
+});
 
 export default class App extends Component {
-  state = {
-    user: {
-      latitude = 0,
-      longitude = 0,
-    }
-  }
-
-  renderAnnotations () {
-    return (
-      <Mapbox.PointAnnotation
-        key='pointAnnotation'
-        id='pointAnnotation'
-        coordinate={[-49.372980, -25.554312]}>
-
-        <View style={styles.annotationContainer}>
-          <View style={styles.annotationFill} />
-        </View>
-        <Mapbox.Callout title='Ponto fixo no mapa' />
-      </Mapbox.PointAnnotation>
-    )
-  }
-
-  _onLongPress(e) {
-    const [latitudeActual, longitudeActual] = e.geometry.coordinates;
-
-    this.setState(
-      {
-        user: {latitude: latitudeActual, longitude: longitudeActual
-      }
-    });
-
-    const { latitude, longitude } = this.state.user;
-
-    //console.log(latitude, longitude);
-  }
-
-  componentDidMount(){
-    PermissionsAndroid.requestMultiple(
-              [PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-              PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION],
-              {
-                  title: 'Permissão de Localização',
-              message: 'O App precisa de permissão de localização para encontrar sua posição'
-          }
-      ).then(granted => {
+  componentDidMount() {
+    if (Platform.OS === "android") {
+      PermissionsAndroid.requestMultiple(
+        [
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
+        ],
+        {
+          title: "Permissão de Localização",
+          message:
+            "O App precisa de permissão de localização para encontrar sua posição"
+        }
+      )
+        .then(granted => {
           //console.log(granted);
           resolve();
-      }).catch(err => {
+        })
+        .catch(err => {
           //console.warn(err);
           //reject(err);
-      });
+        });
+    }
   }
 
   render() {
     return (
-      <Mapbox.MapView
-        onLongPress={this._onLongPress}
-        styleURL={Mapbox.StyleURL.Dark}
-        zoomLevel={15}
-        centerCoordinate={[-49.6451598, -27.2177659]}
-        style={styles.container}
-      >
-
-      </Mapbox.MapView>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: -27.210753,
+            longitude: -49.644183,
+            latitudeDelta: 0.0143,
+            longitudeDelta: 0.0134
+          }}
+          showsUserLocation
+          loadingEnabled
+        />
+      </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  annotationContainer: {
-    width: 30,
-    height: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    borderRadius: 15,
-  },
-  annotationFill: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    backgroundColor: 'orange',
-    transform: [{ scale: 0.6 }],
-  },
-});
