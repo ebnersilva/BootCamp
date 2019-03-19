37671ac6368864a40bd8aff84f2824a43a9b4446
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-19 15:07:45 -0300

    GoNode - modulo4 - Criamos os relacionamentos

--- GoNode/aulas/modulo4/app/Models/Project.js
+++ GoNode/aulas/modulo4/app/Models/Project.js
@@ -4,6 +4,13 @@
 const Model = use('Model')
 
 class Project extends Model {
+  user () {
+    return this.belongsTo('App/Models/User') // Pertence a 1 usuário
+  }
+
+  tasks () {
+    return this.hasMany('App/Models/Task') // Tem muitas tarefas
+  }
 }
 
 module.exports = Project
