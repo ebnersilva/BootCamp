5fb7c057b1cacaaf36882ddfa9ae8ff1b6c2bc2e
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-26 11:18:22 -0300

    Finalizamos o módulo 4 e criamos o projeto do desafio do modulo 4

--- GoNode/aulas/desafiomod4/app/Models/User.js
+++ GoNode/aulas/desafiomod4/app/Models/User.js
@@ -10,27 +10,29 @@ class User extends Model {
   static boot () {
     super.boot()
 
-    this.addHook('beforeSave', async userInstance => {
+    /**
+     * A hook to hash the user password before saving
+     * it to the database.
+     */
+    this.addHook('beforeSave', async (userInstance) => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
     })
   }
 
-  addresses () {
-    return this.hasMany('App/Models/UserAddress')
-  }
-
+  /**
+   * A relationship on tokens is required for auth to
+   * work. Since features like `refreshTokens` or
+   * `rememberToken` will be saved inside the
+   * tokens table.
+   *
+   * @method tokens
+   *
+   * @return {Object}
+   */
   tokens () {
     return this.hasMany('App/Models/Token')
-  }
-
-  projects () {
-    return this.hasMany('App/Models/Project')
-  }
-
-  tasks () {
-    return this.hasMany('App/Models/task')
   }
 }
 
