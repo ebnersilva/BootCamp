ac3c97058e290702a0e1cf721b726265502cf180
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-03-29 15:42:08 -0300

    GoReact - modulo4 - Carregamos as playlists na sidebar

--- GoReact/aulas/modulo4/src/components/Sidebar/index.js
+++ GoReact/aulas/modulo4/src/components/Sidebar/index.js
@@ -1,67 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlaylistsActions } from '../../store/ducks/playlists';
 
 import { Container, Nav, NewPlaylist } from './styles';
 
 import AddPlaylistIcon from '../../assets/images/add_playlist.svg';
 
-const Sidebar = () => (
-  <Container>
-    <div>
-      <Nav main>
-        <li>
-          <a href="http://localhost:3000">Navegar</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Radio</a>
-        </li>
-      </Nav>
-      <Nav>
-        <li>
-          <span>SUA BIBLIOTECA</span>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Seu Daily Mix</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Tocados recentemente</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Músicas</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Albuns</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Artistas</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Estações</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Arquivos locais</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Videos</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Podcasts</a>
-        </li>
-      </Nav>
+class Sidebar extends Component {
+  static propTypes = {
+    getPlaylistsRequest: PropTypes.func.isRequired,
+    playlists: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          title: PropTypes.string,
+        }),
+      ),
+    }).isRequired,
+  };
 
-      <Nav>
-        <li>
-          <span>PLAYLISTS</span>
-        </li>
-        <li>
-          <a href="http://localhost:3000">Melhores do rock</a>
-        </li>
-      </Nav>
-    </div>
-    <NewPlaylist>
-      <img src={AddPlaylistIcon} alt="Adicionar playlist" />
-      Nova playlist
-    </NewPlaylist>
-  </Container>
-);
+  componentDidMount() {
+    this.props.getPlaylistsRequest();
+  }
 
-export default Sidebar;
+  render() {
+    const { playlists } = this.props;
+    return (
+      <Container>
+        <div>
+          <Nav main>
+            <li>
+              <a href="http://localhost:3000">Navegar</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Radio</a>
+            </li>
+          </Nav>
+          <Nav>
+            <li>
+              <span>SUA BIBLIOTECA</span>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Seu Daily Mix</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Tocados recentemente</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Músicas</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Albuns</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Artistas</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Estações</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Arquivos locais</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Videos</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Podcasts</a>
+            </li>
+          </Nav>
+
+          <Nav>
+            <li>
+              <span>PLAYLISTS</span>
+            </li>
+            {playlists.data.map(playlist => (
+              <li key={playlist.id}>
+                <Link to={`playlists/${playlist.id}`}>{playlist.title}</Link>
+              </li>
+            ))}
+          </Nav>
+        </div>
+        <NewPlaylist>
+          <img src={AddPlaylistIcon} alt="Adicionar playlist" />
+          Nova playlist
+        </NewPlaylist>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  playlists: state.playlists,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlaylistsActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Sidebar);
