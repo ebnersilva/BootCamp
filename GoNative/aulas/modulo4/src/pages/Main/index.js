f808c29273c3e0101e443d491cf388263d76c3ca
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-04-09 22:49:14 -0300

    GoNative - modulo4 - Estilizamos a pagina de podcasts e também aprendimos a criar um botão de voltar

--- GoNative/aulas/modulo4/src/pages/Main/index.js
+++ GoNative/aulas/modulo4/src/pages/Main/index.js
@@ -15,6 +15,12 @@ class Main extends Component {
     loadRequest();
   }
 
+  handlePodcastPress = (podcast) => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Podcast', { podcast });
+  };
+
   render() {
     const { podcasts } = this.props;
     return (
@@ -24,7 +30,7 @@ class Main extends Component {
           data={podcasts.data}
           keyExtractor={podcast => String(podcast.id)}
           renderItem={({ item: podcast }) => (
-            <PodCast onPress={() => {}}>
+            <PodCast onPress={() => this.handlePodcastPress(podcast)}>
               <Cover source={{ uri: podcast.cover }} />
               <Info>
                 <Title>{podcast.title}</Title>
