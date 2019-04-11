70637c00b14c876cd1b6db11e80f9d8feab9545b
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-04-11 14:50:31 -0300

    GoNative - modulo4 - Exibindo episódio atual

--- GoNative/aulas/modulo4/src/components/Player/index.js
+++ GoNative/aulas/modulo4/src/components/Player/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import {
   Container,
   CoverBackground,
@@ -11,26 +13,36 @@ import {
   ControllIcon,
 } from './styles';
 
-const Player = () => (
-  <Container>
-    <CoverBackground source={{ uri: 'https://s3-sa-east-1.amazonaws.com/gonative/cover2.png' }} />
+const Player = ({ player, currentEpisode }) => player.current && (
+<Container>
+  <CoverBackground source={{ uri: currentEpisode.artwork }} />
 
-    <EpisodeInfo>
-      <Title>Papercut</Title>
-      <Author>Linkin Park</Author>
-    </EpisodeInfo>
-    <Controls>
-      <ControllButton onPress={() => {}}>
-        <ControllIcon name="skip-previous" />
-      </ControllButton>
-      <ControllButton onPress={() => {}}>
-        <ControllIcon name="play-circle-filled" />
-      </ControllButton>
-      <ControllButton onPress={() => {}}>
-        <ControllIcon name="skip-next" />
-      </ControllButton>
-    </Controls>
-  </Container>
+  <EpisodeInfo>
+    <Title>{currentEpisode.title}</Title>
+    <Author>{currentEpisode.artist}</Author>
+  </EpisodeInfo>
+  <Controls>
+    <ControllButton onPress={() => {}}>
+      <ControllIcon name="skip-previous" />
+    </ControllButton>
+    <ControllButton onPress={() => {}}>
+      <ControllIcon name="play-circle-filled" />
+    </ControllButton>
+    <ControllButton onPress={() => {}}>
+      <ControllIcon name="skip-next" />
+    </ControllButton>
+  </Controls>
+</Container>
 );
 
-export default Player;
+const mapStateToProps = state => ({
+  player: state.player,
+  currentEpisode: state.player.podcast
+    ? state.player.podcast.tracks.find(episode => episode.id === state.player.current)
+    : null,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(Player);
